a7e02311c02157a2682d71340d54f44a946f55f2
csagpro <csagpro@gmail.com>
2017-09-02 10:23:14 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br"><!-- idioma site -->
+	<head>
+		<meta charset="utf-8"><!-- codificação caracteres-->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"><!-- indica para o navegador utilizar a versão mais moderna do IE -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- define largura do conteudo e zoom inicial -->
+
+		<title></title><!-- titulo -->
+		<meta name="description" content=""><!-- descrição até 156 caracteres-->
+		<meta name="robots" content="index, follow"><!-- indexação mecanismos de busca -->
+		<link rel="shortcut icon" href=""><!-- icone aba navegador-->
+		<link rel="alternate" type="application/rss+xml" title="RSS/Feed" href="rss.xml"><!-- feed -->
+
+		<link rel="stylesheet" href="css/style.css"><!-- folha estilo principal - classes com underline-->
+		<link rel="stylesheet" href="https://necolas.github.io/normalize.css/7.0.0/normalize.css"><!-- reset css -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"><!-- bootstrap css - classes com traço-->
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script><!-- bootstrap jquery -->
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script><!-- bootstrap javascript -->
+		<!-- script para rodar html5 no IE9 e anteriores -->
+			<!--[if lt IE 9]>
+				<script src="js/html5shiv.js"></script>
+			<![endif]-->
+
+		<!-- script para aparecer tooltip do bootstrap -->
+		<script>
+			$(document).ready(function(){
+			    $('[data-toggle="tooltip"]').tooltip();
+			});
+		</script>
+
+		<!-- script para aparecer popover do bootstrap -->
+		<script>
+			$(document).ready(function(){
+			    $('[data-toggle="popover"]').popover();
+			});
+		</script>
+
+		<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Spectral|Jura|Quicksand" rel="stylesheet"><!-- fontes google -->
+	</head>
+	<body>
+
+
+		<div class=""></div>
+	</body>
+</html>
